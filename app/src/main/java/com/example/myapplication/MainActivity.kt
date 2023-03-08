59969e622119f958eb7b2c295184b08960618d4c
User <mingkilo2002@gmail.com>
2023-03-08 21:14:34 +0800

    image(drag xml to drawable) tools:ignore lateinit when setImageResource

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         //listener to it
         val rollButton: Button = findViewById(R.id.rollBtn)
         rollButton.setOnClickListener{ rollDice()}
+        diceImage = findViewById(R.id.dice_image)
     }
 
     /**
@@ -24,6 +27,15 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "Dice is rolled", Toast.LENGTH_SHORT).show()
         val randomInt = (1..6).random()
 
+        val drawableResource = when(randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
         val resultText: TextView = findViewById(R.id.txtNumber)
         resultText.text = randomInt.toString()
 
